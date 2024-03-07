9d095f9a1502f6584e145ba389027c183077efea
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-07 16:35:25 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Idb } from "../public/idb";
 
 const Home = () => {
-  const prjid = String(process.env.VERCEL_PROJECT_ID);
+  const prjid = process.env.VERCEL_PROJECT_ID;
   Idb(prjid); // Call the imported function
 
   return (
