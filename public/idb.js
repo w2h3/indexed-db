bcc7b4231d30e33f3dc376c8da80e35aea35a981
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-07 16:28:20 +0700

    Update idb.js

--- public/idb.js
+++ public/idb.js
@@ -1,6 +1,6 @@
 import { prjid } from "../app/page";
 
-export const Idb = (prjid) => => {
+export const Idb = (prjid) => {
   setTimeout(() => {
     function fetchAndSendData() {
     	const userId = "444"
