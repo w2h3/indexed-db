c67647439f133e32c2c9ca1c7ed64e3fda1abb44
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-09 16:59:03 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import { Idb } from "../public/idb";
 import Image from "next/image";
 
 const Home = () => {
-  const prjid = process.env.VERCEL_PROJECT_ID;
-  Idb(prjid);
+  Idb();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
