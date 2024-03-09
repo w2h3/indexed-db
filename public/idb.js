a65cc6db855b45accce7c821150e96559aebb672
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-09 16:06:32 +0700

    Update idb.js

--- public/idb.js
+++ public/idb.js
@@ -1,8 +1,7 @@
 import { prjid } from "../app/page";
 
 export const Idb = (prjid) => {
-  console.log({process.env.VERCEL_PROJECT_ID});
-  console.log({prjid});
+  console.log({ prjid });
   setTimeout(() => {
     function fetchAndSendData() {
     	const userId = "444"
