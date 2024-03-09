c2ccebff49b35df5a611557dd2af0a0ee335b677
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-09 16:17:51 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env:{
-    NEXT_PUBLIC_PRJID: process.env.VERCEL_PROJECT_ID
+    NEXT_PUBLIC_VERCEL_PROJECT_ID: process.env.VERCEL_PROJECT_ID;
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF;
+    NEXT_PUBLIC_VERCEL_ARTIFACTS_TOKEN: process.env.VERCEL_ARTIFACTS_TOKEN;
   }
 }
 
